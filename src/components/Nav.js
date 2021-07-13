2e6c42fdc7e711ab43d14c9933de0d3b37047b44
Adam Marsala <adammarsala@hotmail.com>
2021-07-13 16:07:18 -0400

    Change nav anchor tags to buttons and style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,30 +9,114 @@ const Nav = (props) => {
     position: PropTypes.number,
     scrollTo: PropTypes.func
   };
-  // State of whether to show font awesome bottom border
+
+  // Whether to show font awesome nav icon (hamburger) fa-bars
   const [state, setState] = useState({
-    showNavBorder: false, 
+    showDropdown: false, 
   });
 
-  useEffect(() => {
-    // on first render we are at the top of the page
-    // so we don't show the nav border
-    setState({...state, showNavBorder: false});
-  }, [props.position]);
+  // useEffect(() => {
+  //   setState({...state, showDropdown: false});
+  // }, [props.position]);
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setState({...state, showDropdown: false});
+  //   };
+  //   window.addEventListener("resize", handleResize);
+  // }, []);
+
+  const toggleDropdown = () => {
+    setState({
+      ...state,
+      showDropdown: !state.showDropdown
+    });
+  };
+
+  const handleClick = () => {
+    // upon user clicking a nav heading
+    if (state.showDropdown) {
+      // close the mobile nav menu if is open
+      toggleDropdown();
+    }
+  }
 
 
   return (
     <div className={`Nav`}>
+
+      {/* LOGO */}
       <div className="nav-left">
         <a href="/">
           <img src={logo} alt="Portfolio Logo" />
         </a>
       </div>
-      <i
-        className={`fa fa-bars ${state.showNavBorder ? "active" : ""}`}
-      />
-      <div className="nav-right">
 
+      {/* Menu Bars Icon - only visible in Mobile */}
+      <i
+        className={`fa fa-bars ${state.showDropdown ? "active" : ""}`}
+        onClick={() => toggleDropdown()}
+      />
+
+      {/* Desktop view nav bar */}
+      <div className="nav-right">
+        <button onClick={() => props.scrollTo("about")}>
+          <div className={`nav-item about ${props.view === "about" ? "selected" : ""}`}>
+            ABOUT
+          </div>
+        </button>
+        <button onClick={() => props.scrollTo("skills")}>
+          <div className={`nav-item skills ${props.view === "skills" ? "selected" : ""}`}>
+            SKILLS
+          </div>
+        </button>
+        <button onClick={() => props.scrollTo("projects")}>
+          <div className={`nav-item projects ${props.view === "projects" ? "selected" : ""}`}>
+            PROJECTS
+          </div>
+        </button>
+        <button onClick={() => props.scrollTo("contact")}>
+          <div className={`nav-item contact ${props.view === "contact" ? "selected" : ""}`}>
+            CONTACT
+          </div>
+        </button>
+        <a href="https://ahhreggi.github.io/docs/resume.pdf" target="_blank" rel="noreferrer">
+          <div className="nav-item resume">
+            RESUME
+          </div>
+        </a>
+      </div>
+
+      {/* Mobile view nav bar */}
+      <div className="nav-dropdown">
+        <a href="#about" onClick={() => handleClick()}>
+          <div className={`nav-item about ${props.view === "about" ? "selected" : ""}`}>
+            ABOUT
+          </div>
+        </a>
+        <a href="#skills" onClick={() => handleClick()}>
+          <div className={`nav-item skills ${props.view === "skills" ? "selected" : ""}`}>
+            SKILLS
+          </div>
+        </a>
+        <a href="#projects" onClick={() => handleClick()}>
+          <div className={`nav-item projects ${props.view === "projects" ? "selected" : ""}`}>
+            PROJECTS
+          </div>
+        </a>
+        <a href="#contact" onClick={() => handleClick()}>
+          <div className={`nav-item contact ${props.view === "contact" ? "selected" : ""}`}>
+            CONTACT
+          </div>
+        </a>
+        <a
+          href="#" target="_blank" rel="noreferrer"
+          onClick={() => handleClick()}
+        >
+          <div className="nav-item resume">
+            RESUME
+          </div>
+        </a>
       </div>
 
     </div>
