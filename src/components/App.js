5a10ee9f4daaff0c5ef304f1803dc73770f6cebb
Adam Marsala <adammarsala@hotmail.com>
2021-07-13 15:27:53 -0400

    Set up App and Nav scroll state and handlers

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,53 @@
+import { useState, useEffect, useRef } from "react";
 import Nav from "./Nav";
 
 const App = () => {
+  const [state, setState] = useState({
+    view: "top", // marks top position window scroll 
+    position: 0 // tracks y position of window scroll
+  });
+
+  // React will hold DOM state of portfolio sections in refs
+  // So we can use scrollIntoView with its `.current` property
+  const refs = {
+    about: useRef(null),
+    skills: useRef(null),
+    projects: useRef(null),
+    contact: useRef(null)
+  };
+
+  const scrollTo = (ref) => {
+    refs[ref].current.scrollIntoView();
+  };
+
+  const handleScroll = () => {
+    // Hold Y scroll position of window
+    const pos = window.pageYOffset;
+    let view; 
+    if (pos < 10) {
+      // consider y offset of less than 10 the top
+      view = "top";
+    }
+    // track the Y scroll position in state
+    setState({...state, view: view, position: pos});
+  };
+
+  useEffect(() => {
+    // Setting "passive" to true indicates handleScroll will never call preventDefault()
+    // to improve scroll performance on browsers other than Chrome and Firefox
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  });
+
   return (
     <div className="App">
-      <Nav />
-      <p>Hello, my name is Adam Joseph Marsala</p>
+      <Nav 
+        view={state.view}
+        position={state.position}
+        scrollTo={scrollTo}
+      />
 
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,38 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import "./Nav.scss";
 import logo from "../images/logo.png";
 
-const Nav = () => {
+const Nav = (props) => {
+  Nav.propTypes = {
+    view: PropTypes.string,
+    position: PropTypes.number,
+    scrollTo: PropTypes.func
+  };
+  // State of whether to show font awesome bottom border
+  const [state, setState] = useState({
+    showNavBorder: false, 
+  });
+
+  useEffect(() => {
+    // on first render we are at the top of the page
+    // so we don't show the nav border
+    setState({...state, showNavBorder: false});
+  }, [props.position]);
+
+
   return (
     <div className={`Nav`}>
       <div className="nav-left">
         <a href="/">
           <img src={logo} alt="Portfolio Logo" />
         </a>
+      </div>
+      <i
+        className={`fa fa-bars ${state.showNavBorder ? "active" : ""}`}
+      />
+      <div className="nav-right">
+
       </div>
 
     </div>
